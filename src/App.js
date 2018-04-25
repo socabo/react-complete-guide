ecdb18ef7769d66ebf644c6de2771ed8b48a4db8
Ilidio Lopes <depil01@ca.com>
2018-04-25 16:59:24 +0200

    56. Lists & Keys

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Person from './Person/Person'
 class App extends Component {
   state = {
     persons: [
-      {name: 'Max', age: '28'},
-      {name: 'Manu', age: '29'},
-      {name: 'Stephanie', age: '26'}
+      {id: 'jhgf', name: 'Max', age: '28'},
+      {id: 'iuy', name: 'Manu', age: '29'},
+      {id: 'fdsg', name: 'Stephanie', age: '26'}
     ],
     otherState: 'some other value',
     showPersons: false
@@ -53,7 +53,8 @@ class App extends Component {
             return <Person
               click={() => this.deletePersonHandler(index)} 
               name = {person.name}
-              age =  {person.age} />
+              age =  {person.age}
+              key={person.id} />
           })}
         </div>
       )
